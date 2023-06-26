ab7d4d34ecec1a435a443a0a2f44a865366d0a54
Kirill Blazheev <blazheev.k@gmail.com>
2023-06-26 16:15:48 +0300

    modified:   hw2.py

--- hw2.py
+++ hw2.py
@@ -1,17 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-url = "https://api.hh.ru/vacancies?text=middle python разработчик&per_page=10"
+url = "https://api.hh.ru/vacancies?text=middle python разработчик&per_page=100"
 result = requests.get(url)
 if result.status_code == 200:
     vacancies = result.json().get('items')
     for i, vacancy in enumerate(vacancies):
-        if 'snippet' in vacancy.keys():
-            if ('responsibility' in vacancy['snippet'].keys() and vacancy['snippet']['responsibility'] != None):
-                responsibilitiy = BeautifulSoup(vacancy['snippet']['responsibility'], 'lxml').get_text()
-            else:
-                responsibilitiy = ''
-            if ('requirement' in vacancy['snippet'].keys() and vacancy['snippet']['requirement'] != None):
-                reqirements = BeautifulSoup(vacancy['snippet']['requirement'], 'lxml').get_text()
-            else:
-                reqirements = ''
-            print(i + 1, vacancy['employer']['name'], vacancy['name'], responsibilitiy, reqirements, sep = '\n')
+        responsibilitiy = ''
+        reqirements = ''
+        if (vacancy['snippet']['responsibility'] != None):
+            responsibilitiy = BeautifulSoup(vacancy['snippet']['responsibility'], 'lxml').get_text()
+        if (vacancy['snippet']['requirement'] != None):
+            reqirements = BeautifulSoup(vacancy['snippet']['requirement'], 'lxml').get_text() 
+        print(i + 1, vacancy['employer']['name'], vacancy['name'], responsibilitiy, reqirements, sep = '\n')
