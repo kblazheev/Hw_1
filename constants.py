8806e6573dce43cfad0d5103c3f1f631114c87bf
Kirill Blazheev <blazheev.k@gmail.com>
2023-06-27 14:35:14 +0300

    Changes to be committed: 	modified:   constants.py 	deleted:    hw1.db 	modified:   hw2.py

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ okved_archive = 'okved_2.json.zip'
 okved_file = 'okved_2.json'
 egrul_archive = 'egrul.json.zip'
 db = 'hw.db'
+url = "https://api.hh.ru/vacancies?text=middle+python+разработчик&per_page=100&search_field=name"
--- hw2.py
+++ hw2.py
@@ -1,14 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
-url = "https://api.hh.ru/vacancies?text=middle python разработчик&per_page=100"
-result = requests.get(url)
-if result.status_code == 200:
-    vacancies = result.json().get('items')
-    for i, vacancy in enumerate(vacancies):
-        responsibilitiy = ''
-        reqirements = ''
-        if (vacancy['snippet']['responsibility'] != None):
-            responsibilitiy = BeautifulSoup(vacancy['snippet']['responsibility'], 'lxml').get_text()
-        if (vacancy['snippet']['requirement'] != None):
-            reqirements = BeautifulSoup(vacancy['snippet']['requirement'], 'lxml').get_text() 
-        print(i + 1, vacancy['employer']['name'], vacancy['name'], responsibilitiy, reqirements, sep = '\n')
+import sqlite3 as sql
+from constants import *
+
+try:
+    search_result = requests.get(url)
+    if search_result.status_code == 200:
+        vacancy_list = search_result.json().get('items')
+        data = []
+        for item in vacancy_list:
+            vacancy_page = requests.get(item['url'])
+            if vacancy_page.status_code == 200:
+                vacancy = vacancy_page.json()
+                description = ''
+                key_skills = ''
+                if (vacancy['description'] != None):
+                    description = BeautifulSoup(vacancy['description'], 'lxml').get_text()
+                if (vacancy['key_skills'] != None):
+                    for skill in vacancy['key_skills']:
+                        key_skills += skill['name'] + ', '
+                    key_skills = key_skills[:-2]
+                data.append([vacancy['employer']['name'], vacancy['name'], description, key_skills])
+        try:
+            connection = sql.connect(db)
+            cursor = connection.cursor()
+            print(f"База данных {db} подключена к SQLite")
+            tab_create = '''
+            CREATE TABLE IF NOT EXISTS vacancies(
+                id integer primary key,
+                employer_name text,
+                name text,
+                description text,
+                key_skills text
+            );'''
+            cursor.execute(tab_create)
+            connection.commit()
+            print(f"Таблица {tab_create}\n успешно добавлена в БД")
+            insert_val = "INSERT INTO vacancies(employer_name, name, description, key_skills) VALUES(?, ?, ?, ?);"
+            cursor.executemany(insert_val, data)
+            connection.commit()
+            print(f"Данные успешно добавлены в таблицу")
+            cursor.close()
+        except sql.Error as error:
+            print(f"Не удалось вставить данные в таблицу")
+            print("Исключение: ", error.__class__, error.args)
+        finally:
+            if (connection):
+                connection.close()
+                print("Соединение с SQLite закрыто")
+    else:
+        print(f'Не удалось загрузить данные поиска: {search_result.status_code}')
+except:
+    print('Отсутствует сетевое подключение')
