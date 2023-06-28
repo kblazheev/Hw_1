cb0b23eb2d6b1625d5921823faa83cb8e660cecb
Kirill Blazheev <blazheev.k@gmail.com>
2023-06-28 17:39:21 +0300

    Changes to be committed: 	modified:   constants.py 	modified:   hw.db 	new file:   hw2_parse.py

--- constants.py
+++ constants.py
@@ -2,4 +2,6 @@ okved_archive = 'okved_2.json.zip'
 okved_file = 'okved_2.json'
 egrul_archive = 'egrul.json.zip'
 db = 'hw.db'
-url = "https://api.hh.ru/vacancies?text=middle+python+разработчик&per_page=100&search_field=name"
+url = "https://api.hh.ru/vacancies?text=middle+python+developer&area=1&per_page=100&search_field=name"
+url2 = "https://hh.ru/search/vacancy?text=middle+python+developer&area=1&items_on_page=100&search_field=name"
+user_agent = {'User-agent': 'Mozilla/5.0'}
